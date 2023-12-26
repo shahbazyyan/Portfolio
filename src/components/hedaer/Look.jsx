acbe4e5a1ca622bac965672aa8ceeb84ac77393d
David Shahbazyan Mushegh <david.shahbazyan.m@totogaming.am>
2023-12-26 14:22:32 +0400

    Created cv update

--- src/components/hedaer/Look.jsx
+++ src/components/hedaer/Look.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import CV from '../../assets/CV.pdf';
+import CV from '../../assets/cv.pdf.pdf';
 
 function Look() {
   return (
     <div className='look'>
         <a href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
-        {/* <a href="#" className='btn'>View Certificate</a> */}
+        
     </div>
   )
 }
